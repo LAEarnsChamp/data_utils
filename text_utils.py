9cd7a06e83f0cc5af77d6ce9f4beda06e7648337
sivous <540317156@qq.com>
2020-06-22 21:06:25 +0800

    add: get stopwords from file and list

--- text_utils.py
+++ text_utils.py
@@ -11,16 +11,37 @@ if not os.path.exists(foldername):
     os.makedirs(foldername)
 
 
-def gensim_text2corpus(text, cuttool="jieba",
-                        stopwords="cn_stopwords",
-                        self_stopwords=[]):
+def getstopwords(stopwords="./", addition=None):
     '''
-    transform text to gensim's corpus
+    stopwords: stopwords file path
+    addition: stopwords list or None
+
+    return: list of stopwords
+    '''
+    if isinstance(addition, list) is not True and addition is not None:
+        raise TypeError("argument 'addition' must be string list or None")
+    # stopwords list
+    list_sw = []
+
+    # if stopwords file is not found
+    try:
+        with open("{}".format(stopwords), 'r', encoding='UTF-8') as sw_f:
+            for line in sw_f.readlines():
+                list_sw.append(line.strip())
+        list_sw += addition
+    except FileNotFoundError as e:
+        raise e
+    return list_sw
+
+
+def experiment_gensim_text2corpus(text, cuttool="jieba", stopwords="cn_stopwords", self_stopwords=[]):
+    '''
+    quick experiment for transform text to gensim's corpus
 
     text: input text, shape is (texts num, text)
     cuttool: the tool for cutting text into words
     stopwords: list of stopwords,
-    can be "cn_stopwords"/"hit_stopwords"/"baidu_stopwords"/"scu_stopwords"
+    can be "cn_stopwords"/"hit_stopwords"/"baidu_stopwords"/"scu_stopwords" or None
     self_stopwords: list of stopwords from yourself
 
     return: dictionary, corpus
@@ -29,42 +50,44 @@ def gensim_text2corpus(text, cuttool="jieba",
     from gensim import corpora
     import urllib
 
+    def cut_jieba(texts, list_sw):
+        import jieba
+        words_ls = []
+        for text in texts:
+            words_ls.append([word for word in jieba.lcut(text) if word not in list_sw])
+        return words_ls
+
     cuttools = {"jieba": cut_jieba}
     # get stopwords list
     list_sw = []
-    stopwords_files_download = {
-        "cn_stopwords": "https://github.com/goto456/stopwords/raw/master/cn_stopwords.txt"
-    }
-    # if stopwords file is not found
-    if not os.path.exists("C:\\Users\\{}\\.datautils\\stopwords\\{}.txt".format(getpass.getuser(), stopwords)):
-        print("Downloading stopwords file")
-        urllib.request.urlretrieve(stopwords_files_download[stopwords],
-                   "C:\\Users\\{}\\.datautils\\stopwords\\{}.txt".format(getpass.getuser(), stopwords),
-                   lambda num, size, all_: print(num * size / all_))
-        print("Download completely")
 
-    # if existed
-    with open("C:\\Users\\{}\\.datautils\\stopwords\\{}.txt".format(getpass.getuser(), stopwords),
-                'r',
-                encoding='UTF-8') as sw_f:
-        for line in sw_f.readlines():
-            list_sw.append(line.strip())
+    if stopwords is not None:
+        stopwords_files_download = {
+            "cn_stopwords": "https://github.com/goto456/stopwords/raw/master/cn_stopwords.txt"
+        }
+        # if stopwords file is not found
+        if not os.path.exists("C:\\Users\\{}\\.datautils\\stopwords\\{}.txt".format(getpass.getuser(), stopwords)):
+            print("Downloading stopwords file")
+            urllib.request.urlretrieve(stopwords_files_download[stopwords],
+                       "C:\\Users\\{}\\.datautils\\stopwords\\{}.txt".format(getpass.getuser(), stopwords),
+                       lambda num, size, all_: print(num * size / all_))
+            print("Download completely")
+        # if stopwords file existed
+        with open("C:\\Users\\{}\\.datautils\\stopwords\\{}.txt".format(getpass.getuser(), stopwords),
+                    'r',
+                    encoding='UTF-8') as sw_f:
+            for line in sw_f.readlines():
+                list_sw.append(line.strip())
+    else:
+        pass
 
     # cut word with stop words list
     list_sw.extend(self_stopwords)
     words_ls = cuttools[cuttool](text, list_sw)
 
     dictionary = corpora.Dictionary(words_ls)
-    # 基于词典，使【词】→【稀疏向量】，并将向量放入列表，形成【稀疏向量集】
     corpus = [dictionary.doc2bow(words) for words in words_ls]
 
     return dictionary, corpus
 
 
-def cut_jieba(texts, list_sw):
-    import jieba
-
-    words_ls = []
-    for text in texts:
-        words_ls.append([word for word in jieba.lcut(text) if word not in list_sw])
-    return words_ls
